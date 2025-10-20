8893c8c96fc3ea1754ceed176a944015ed60a753
Paweł Krok <pkrok@hotmail.com>
2025-10-20 18:59:22 +0200

    chore(firestore): Update database rules

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -13,6 +14,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseFirestore.setLoggingEnabled(true);
   Intl.defaultLocale = 'pl_PL';
 
   runApp(const ProviderScope(
